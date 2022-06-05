8e61c644023e970f05982b088d8680ee4170d6a3
Tom <thomas.andrew.hansen@gmail.com>
2022-06-05 12:11:22 +1000

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+import React from "react";
+
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  <Elements stripe={stripePromise}>
+            <Component {...pageProps} />
+          </Elements>
+
 }
 
 export default MyApp
